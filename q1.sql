515a509deba418c06c470a2fe9bb7c546d652e69
Gregory Van Horne <gvanhorne@telus.net>
2023-10-28 15:21:00 -0700

    Add production type count and lifetime productions

--- q1.sql
+++ q1.sql
@@ -1,5 +1,5 @@
-WITH max_2002_movies AS (
-  SELECT pid, count(pid) as n2002
+WITH movie_directors_2002 AS (
+  select pid, count(pid) as n2002
   FROM (
     select pid, id
     FROM persons AS p
@@ -12,21 +12,40 @@ WITH max_2002_movies AS (
   GROUP BY pid
 ),
 
-director_names AS (
+most_prolific_2002_movie_directors AS (
+  SELECT * from movie_directors_2002
+  WHERE n2002 = (SELECT MAX(n2002) FROM movie_directors_2002)
+),
+
+prolific_directors_names AS (
   select pid, personname
   FROM persons
   WHERE pid
   IN (
     SELECT pid
-    FROM max_2002_movies
+    FROM most_prolific_2002_movie_directors
   )
+),
+
+lifetime_productions AS (
+  select pid, id
+  FROM most_prolific_2002_movie_directors as p
+  JOIN directors as d
+  ON p.pid = d.director
+),
+
+productions_by_type AS (
+  select l.pid, p.productiontype, count(productiontype) as n
+  FROM productions as p
+  JOIN lifetime_productions as l
+  ON p.id = l.id
+  GROUP by l.pid, p.productiontype
 )
--- SELECT m.pid, personname, productiontype, n2002, n
-SELECT m.pid, personname, n2002
+
+SELECT
+  n.pid, n.personname, t.productiontype, m.n2002, t.n
 FROM (
-  max_2002_movies AS m
-  INNER JOIN
-  director_names AS p
-  ON p.pid = m.pid
-  )
-WHERE n2002 = (SELECT MAX(n2002) FROM max_2002_movies);
+  productions_by_type as t
+  JOIN prolific_directors_names AS n ON t.pid = n.pid
+  JOIN movie_directors_2002 as m ON t.pid = m.pid
+);
