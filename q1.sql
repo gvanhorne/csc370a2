50ef1f1acd746f4be41e59936b07bc24ae1359e9
Gregory Van Horne <gvanhorne@telus.net>
2023-10-27 23:07:34 -0700

    Display only directors with max number of directed movies in 2002

--- q1.sql
+++ q1.sql
@@ -1,10 +1,27 @@
--- Select all crew appearances for a given director
-SELECT *
-FROM crew
-WHERE pid
-IN (
-  SELECT director
-  FROM directors
+WITH max_2002_movies AS (
+  SELECT pid, count(pid) as n2002
+  FROM (
+    select pid, id
+    FROM persons AS p
+    JOIN directors AS d
+    ON p.pid = d.director
+  ) AS c
+  JOIN productions AS p
+  ON p.id = c.id
+  WHERE productiontype = 'movie' AND year = '2002'
+  GROUP BY pid
+),
+
+director_names AS (
+  select pid, personname
+  FROM persons
+  WHERE pid
+  IN (
+    SELECT pid
+    FROM max_2002_movies
+  )
 )
-and crewtype = 'director'
-limit 10;
+
+SELECT m.pid, personname, n2002
+FROM (max_2002_movies as m INNER JOIN director_names as p ON p.pid = m.pid)
+WHERE n2002 = (SELECT MAX(n2002) FROM max_2002_movies);
