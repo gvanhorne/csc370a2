e074dc0b8b7a5ab6d8bfb1514c6bcbdfd16ff77a
Gregory Van Horne <gvanhorne@telus.net>
2023-10-28 21:56:22 -0700

    Fix sumvotes attribute name

--- q2.sql
+++ q2.sql
@@ -71,13 +71,19 @@ ep_per_season AS (
 ),
 
 sum_ratings AS (
-  SELECT season, SUM(numvotes) as withratings
+  SELECT season, SUM(numvotes) as sumvotes
   FROM ratings as r JOIN all_episode_ids as i ON r.id = i.id
   GROUP BY season
 )
 
+-- avg_ratings AS (
+--   SELECT season, count(PARTITION by season) as 
+-- )
+
+-- select * from avg_ratings;
+
 -- starting to build final relation...
-SELECT DISTINCT t.title, f.season, y.first_year, nepisodes, withratings
+SELECT DISTINCT t.title, f.season, y.first_year, nepisodes, sumvotes
 FROM
   (
     filtered_seasons AS f
