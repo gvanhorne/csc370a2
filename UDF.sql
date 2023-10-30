c46f9e962629cac55a7ea9666122a20a7a3163b2
Gregory Van Horne <gvanhorne@telus.net>
2023-10-29 22:36:55 -0700

    Move query into reusable function

--- UDF.sql
+++ UDF.sql
@@ -9,41 +9,41 @@ CREATE TYPE series_detail AS (
   votes integer
 );
 
--- CREATE OR REPLACE FUNCTION user014_series(title text)
--- RETURNS series_detail AS $$
---   SELECT * from productions as p where p.title = title;
--- $$ LANGUAGE SQL;
+CREATE OR REPLACE FUNCTION user014_series(title text)
+RETURNS series_detail AS $$
+  WITH series_id AS (
+    SELECT *
+    FROM productions as p
+    JOIN episodes as e
+    ON p.id = e.episodeof
+    WHERE p.title = 'Star Trek'
+    AND productiontype = 'tvSeries'
+  ),
 
-WITH series_id AS (
-  SELECT *
-  FROM productions as p
-  JOIN episodes as e
-  ON p.id = e.episodeof
-  WHERE p.title = 'Star Trek'
-  AND productiontype = 'tvSeries'
-),
+  nseasons AS (
+    SELECT episodeof, COUNT(DISTINCT season) as nseasons
+    FROM series_id
+    GROUP BY episodeof
+  ),
 
-nseasons AS (
-  SELECT episodeof, COUNT(DISTINCT season) as nseasons
-  FROM series_id
-  GROUP BY episodeof
-),
+  nepisodes AS (
+    SELECT episodeof, COUNT(*) as nepisodes
+    FROM series_id as s
+    GROUP BY episodeof
+  ),
 
-nepisodes AS (
-  SELECT episodeof, COUNT(*) as nepisodes
-  FROM series_id as s
-  GROUP BY episodeof
-),
+  rating_info AS (
+    SELECT episodeof, averagerating as avgrating, numvotes as votes
+    FROM (
+      SELECT DISTINCT episodeof, numvotes, averagerating FROM series_id as s
+      JOIN ratings as r ON r.id = s.episodeof
+    ) as c
+  )
 
-rating_info AS (
-  SELECT episodeof, averagerating as avgrating, numvotes as votes
-  FROM (
-    SELECT DISTINCT episodeof, numvotes, averagerating FROM series_id as s
-    JOIN ratings as r ON r.id = s.episodeof
-  ) as c
-)
+  select DISTINCT title, year, ns.nseasons, ne.nepisodes, runtime, avgrating, votes
+  FROM series_id as s JOIN nseasons as ns ON s.episodeof = ns.episodeof
+  JOIN nepisodes as ne ON ne.episodeof = s.episodeof
+  JOIN rating_info as ar ON s.episodeof = ar.episodeof;
+$$ LANGUAGE SQL;
 
-select DISTINCT title, year, ns.nseasons, ne.nepisodes, runtime, avgrating, votes
-FROM series_id as s JOIN nseasons as ns ON s.episodeof = ns.episodeof
-JOIN nepisodes as ne ON ne.episodeof = s.episodeof
-JOIN rating_info as ar ON s.episodeof = ar.episodeof;
+-- SELECT * FROM user014_series('Star Trek');
